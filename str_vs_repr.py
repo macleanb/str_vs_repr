08b119748d0b0db707f9479349109607e756799c
Brian MacLean <brian_maclean@hotmail.com>
2024-02-07 14:40:21 -0600

    updated

--- str_vs_repr.py
+++ str_vs_repr.py
@@ -1,16 +1,34 @@
-class my_class():
-    def __init__(self):
-        self._name = "my class"
+class class_with_repr():
+    def __init__(self, name):
+        self._name = name
 
     def __str__(self):
         return f'dunder str was called on {self._name}'
 
     def __repr__(self):
-        return f'dunder repr was called on my_class({self._name})'
+        return f'dunder repr was called on class_with_repr({self._name})'
+
+class class_without_repr():
+    def __init__(self, name):
+        self._name = name
+
+    def __str__(self):
+        return f'dunder str was called on {self._name}'
 
 
-obj = my_class()
-print(obj) # -> 'dunder str was called on my class'
-print(str(obj)) # -> 'dunder str was called on my class'
-print(f'{obj}') # -> 'dunder str was called on my class'
-print(repr(obj)) # -> 'dunder repr was called on my_class(my class)'
+repr_obj_one = class_with_repr('Object 1')
+repr_obj_two = class_with_repr('Object 2')
+print(repr_obj_one) # -> 'dunder str was called on Object 1'
+print(str(repr_obj_one)) # -> 'dunder str was called on Object 1'
+print(f'{repr_obj_one}') # -> 'dunder str was called on Object 1'
+print(repr(repr_obj_one)) # -> 'dunder repr was called on class_with_repr(Object 1)'
+my_list = [repr_obj_one, repr_obj_two] # -> '[dunder repr was called on class_with_repr(Object 1), dunder repr was called on class_with_repr(Object 2)]'
+print(my_list)
+
+no_repr_obj = class_without_repr('Object 3')
+print(no_repr_obj) # -> 'dunder str was called on Object 3'
+print(str(no_repr_obj)) # -> 'dunder str was called on Object 3'
+print(f'{no_repr_obj}') # -> 'dunder str was called on Object 3'
+print(repr(no_repr_obj)) # -> '<__main__.class_without_repr object at 0x100c35710>'
+my_list = [no_repr_obj] # -> '[<__main__.class_without_repr object at 0x100c35710>]'
+print(my_list)
